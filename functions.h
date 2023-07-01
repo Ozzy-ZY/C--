798906a30a49ecfb40f6588902541cffa36d1cc3
Ozzy-ZY <zyadhimself1@gmail.com>
2023-07-01 17:08:55 +0300

    adding some user friendly experience

--- functions.h
+++ functions.h
@@ -6,12 +6,28 @@
 #include "filesystem"
 #include "string"
 #include "fstream"
+#include "ctime"
+#include <chrono>
+#include <variant>
 
 using namespace std;
 random_device seed;
-#define Fast std::ios_base::sync_with_stdio(false);std::cin.tie(nullptr);std::cout.tie(nullptr);
+#define Fast std::ios_base::sync_with_stdio(false);
 #define Srengine default_random_engine engine (seed());
-string hardPass(short numOfChars){
+
+inline string getCurrentTime() {
+    chrono::system_clock::time_point now = chrono::system_clock::now();
+    time_t currentTime = chrono::system_clock::to_time_t(now);
+    tm* timeInfo = localtime(&currentTime);
+
+    ostringstream time;
+    time << setfill('0');
+    time << setw(2) << timeInfo->tm_hour << ":"<< setw(2) << timeInfo->tm_min << ":"
+    << setw(2) << timeInfo->tm_sec;
+
+    return time.str();
+}
+inline string hardPass(short numOfChars){
     Srengine
     uniform_int_distribution<short> randomInt(33,126);
     string pass;
@@ -20,10 +36,11 @@ string hardPass(short numOfChars){
     }
     return pass;
 }
-string midPass(short numOfChars){
+inline string midPass(short numOfChars){
     Srengine
     uniform_int_distribution<short>randomInt1(48,57);
     uniform_int_distribution<short>randomInt2(97,100);
+    
     string pass;
     int rem = numOfChars - (numOfChars / 2);
     for(short i = 0 ; i < numOfChars / 2 ; i++){
@@ -34,7 +51,7 @@ string midPass(short numOfChars){
     }
     return pass;
 }
-string ezPass(short numOfChars){
+inline string ezPass(short numOfChars){
     Srengine
     uniform_int_distribution<short>randomInt1(48,53);
     uniform_int_distribution<short>randomInt2(56,57);
@@ -48,12 +65,12 @@ string ezPass(short numOfChars){
     }
     return pass;
 }
-void writeToFile(const string& password,const string& fileName){
+inline void writeToFile(const string& password,const string& fileName){
     filesystem::path currentPath = filesystem::current_path();
     string path = currentPath.string()+"\\"+fileName;
-    ofstream vault(path,std::ios::app);
+    ofstream vault(path, ios::app);
     if(vault.is_open()){
-        vault<<password<<endl;
+        vault<<password<<"  Generated at=>> "<<getCurrentTime()<<endl;
         vault.close();
         cout<<"---Successful---";
     }
--- program.cpp
+++ program.cpp
@@ -1,16 +1,33 @@
 #include "functions.h"
+#include <regex>
+#include <string>
 using namespace std;
-#define Fast std::ios_base::sync_with_stdio(false);std::cin.tie(nullptr);std::cout.tie(nullptr);
-int main(int argc, char** argv) {
+int main(int argc,char** argv) {
     Fast
-    // ADD user experience
-    // ADD Query System to get password of the specific account and put it in the clipboard
     string fileName;
     short numOfChars;
-    cin>>numOfChars;
+    cout<<"__Welcome to passgenOZ__\n enter the filename you want to save your password at: ";
     cin>>fileName;
-    const string pass = midPass(numOfChars);
-    writeToFile(pass,fileName);
-
+    cout<<"enter the desired Length: ";
+    cin>>numOfChars;
+    cout<<"How hard do you want it bae\n Africa->1\n America->2\nAsia->3\n";
+    short hardness;
+    cin>>hardness;
+    if(hardness == 1){
+        const string pass = hardPass(numOfChars);
+        writeToFile(pass, fileName);
+    }
+    else if (hardness == 2){
+        const string pass = midPass(numOfChars);
+        writeToFile(pass, fileName);
+    }
+    else if (hardness == 3) {
+        const string pass = ezPass(numOfChars);
+        writeToFile(pass, fileName);
+    }
+    else
+    cout<<"U entered a wrong character!!";
+    // ADD user experience
+    // ADD Query System to get password of the specific account and put it in the clipboard
     return 0;
 }
