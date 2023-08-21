179c7133ebf6004faa07fbb652e1d6dedbc7035a
OZZY <Zyadhimself1@gmail.com>
2023-08-22 01:30:35 +0300

    Update program.cpp

--- program.cpp
+++ program.cpp
@@ -1,30 +1,30 @@
 #include "functions.h"
 using namespace std;
-
 unordered_map<string, string> name_pass;
 
-string string_to_lower(string given){
+string string_to_lower(string given) {
     string lower = given;
-    for(short i =0;i<given.size();i++){
-       lower[i] = tolower(given[i]);
+    for (short i = 0; i < given.size(); i++) {
+        lower[i] = tolower(given[i]);
     }
     return lower;
 }
-void normWay(string name){
+
+void normWay(string name) {
     string fileName;
     short numOfChars;
-    cout<<"__Welcome to passgenOZ__\n enter the filename you want to save your password at: ";
-    cin>>fileName;
-    cout<<"enter the desired Length: ";
-    cin>>numOfChars;
-    cout<<"How hard do you want it bae\n*Africa->1\n*America->2\n*Asia->3\n";
+    cout << "__Welcome to passgenOZ__\n enter the filename you want to save your password at: ";
+    cin >> fileName;
+    cout << "enter the desired Length: ";
+    cin >> numOfChars;
+    cout << "How hard do you want it bae\n*Africa->1\n*America->2\n*Asia->3\n";
     short hardness;
-    cin>>hardness;
-    if(hardness == 1){
+    cin >> hardness;
+    if (hardness == 1) {
         const string pass = hardPass(numOfChars);
-        writeToFile(name,pass, fileName);
+        writeToFile(name, pass, fileName);
     }
-    else if (hardness == 2){
+    else if (hardness == 2) {
         const string pass = midPass(numOfChars);
         writeToFile(name, pass, fileName);
     }
@@ -33,41 +33,44 @@ void normWay(string name){
         writeToFile(name, pass, fileName);
     }
     else
-    cout<<"U entered a wrong character!!";
+        cout << "U entered a wrong character!!";
 }
-void queryway(string name){
+
+void queryway(string name) {
     name = string_to_lower(name);
 }
-int main(int argc,char** argv) {
-    if(argc!=3){
-        cout<<"Error: please Enter valid arguments\n-q query\n-a newname\n";
-        return 0;   
+
+int main(int argc, char** argv) {
+    if (argc != 3) {
+        cout << "Error: please Enter valid arguments\n-q query\n-a newname\n";
+        return 0;
     }
     Fast
-    string qr = "-q";
+        string qr = "-q";
     string add = "-a";
-    if(qr.compare(argv[1]) == 0){
-        if(!argv[2]){
-            cout<<"error: You have to add a query!\n";
+    if (qr.compare(argv[1]) == 0) {
+        if (!argv[2]) {
+            cout << "error: You have to add a query!\n";
             return 0;
         }
-        else{
-        queryway(argv[2]);
+        else {
+            queryway(argv[2]);
         }
     }
-    else if(add.compare(argv[1]) == 0){
-        if(!argv[2]){
-            cout<<"error: You have to add a name!\n";
+    else if (add.compare(argv[1]) == 0) {
+        if (!argv[2]) {
+            cout << "error: You have to add a name!\n";
             return 0;
         }
         else {
             normWay(argv[2]);
-        } 
+        }
     }
     else {
-    cout<<"Error: please Enter valid arguments\n-q query\n-a newname\n";
+        cout << "Error: please Enter valid arguments\n-q query\n-a newname\n";
     }
     // ADD user experience
     // ADD Query System to get password of the specific account and put it in the clipboard
     return 0;
 }
+
